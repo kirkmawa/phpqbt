b9b0a94bb86964824b2517c99f547658787bd406
Drew Kirkman <kirkman.w@gmail.com>
2012-12-27 16:21:02 -0600

    change db.php to obey config values

--- EMWIN/lib/db.php
+++ EMWIN/lib/db.php
@@ -3,8 +3,8 @@
 // Database query library
 print ("database: Database library loaded.\r\n");
 
-$db0 = mysql_connect ("localhost", "user", "password");
-mysql_select_db ("phpqbt");
+$db0 = mysql_connect ($config['db']['host'], $config['db']['user'], $config['db']['password']);
+mysql_select_db ($config['db']['db']);
 
 function qbtdbq ($qry) {
 	return (mysql_query ($qry));
