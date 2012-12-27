55553b1cb45270c473f2838d3904e92d476606b3
Drew Kirkman <kirkman.w@gmail.com>
2012-12-27 16:57:24 -0600

    remove log server; remove the assumption this was one's home directory

--- emwin.sh
+++ emwin.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-killall nc
-nohup tail -F emlog.txt | nc -k -l 9944 &
 while true; do
 killall emwin.php
 killall speedcheck.php
-cd
 mysql -u user -ppassword < misc/resetspeedcheck.sql
 echo "starting EMWIN..."
-EMWIN/emwin.php > ~/emlog.txt
+php EMWIN/emwin.php > ./emlog.txt
 done
