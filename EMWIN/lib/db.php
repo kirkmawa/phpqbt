9915b52962832cfc6143e7e88fa22c29c1b46a83
Drew Kirkman <kirkman.w@gmail.com>
2012-12-28 00:51:19 -0600

    switch db.php from connecting to mysql to initializing a mysqli object

--- EMWIN/lib/db.php
+++ EMWIN/lib/db.php
@@ -3,11 +3,6 @@
 // Database query library
 print ("database: Database library loaded.\r\n");
 
-$db0 = mysql_connect ($config['db']['host'], $config['db']['user'], $config['db']['password']);
-mysql_select_db ($config['db']['db']);
-
-function qbtdbq ($qry) {
-	return (mysql_query ($qry));
-	}
+$qbtdb = new mysqli ($config['db']['host'], $config['db']['user'], $config['db']['password'], $config['db']['db']);
 
 ?>
